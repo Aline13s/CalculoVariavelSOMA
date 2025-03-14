e299ec5b83fa3837b9901662729cf54a1bced9bf
Aline13s <128413126+Aline13s@users.noreply.github.com>
2025-03-14 15:05:01 -0300

    Calculo

--- CalculoVariavelSOMA/Program.cs
+++ CalculoVariavelSOMA/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int INDICE = 13, SOMA = 0, K = 0;
+
+        while (K < INDICE)
+        {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        Console.WriteLine($"O valor final da variável SOMA é: {SOMA}");
+    }
+}
